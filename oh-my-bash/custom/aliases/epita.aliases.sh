e1bec57d52afd6bca1eea46247c0e2be50bc3bdf
Lucas STEPHAN <lucas.stephan@epita.fr>
2024-09-23 20:27:30 +0200

    add: custom custom modules

---
+++ oh-my-bash/custom/aliases/epita.aliases.sh
@@ -0,0 +1,5 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+
+alias authors="echo -e 'Lucas\nStephan\nlucas.stephan\nlucas.stephan@epita.fr' > AUTHORS"
+# alias practical-gen="bun run /home/matiix310/perso/bordel_epita/debug-pro/index.ts"
---
+++ oh-my-bash/custom/aliases/perso.aliases.sh
@@ -0,0 +1,6 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+
+alias vencord-installer='sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"'
+alias ssh='kitty +kitten ssh'
+alias codium='/usr/bin/codium --ozone-platform=wayland'
