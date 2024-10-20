a0444fa3d474eac31a1162ad0fbc53f3c1866008
Lucas STEPHAN <lucas.stephan@epita.fr>
2024-10-21 00:51:18 +0200

    update: waybar

--- waybar/scripts/playerctl/playerctl.sh
+++ waybar/scripts/playerctl/playerctl.sh
@@ -3,11 +3,11 @@
 playerctl_status=$(playerctl status 2>/dev/null)
 
 if [[ $playerctl_status == "Playing" ]]; then
-    title=$(playerctl metadata title 2>/dev/null)
-    echo '{"text":"󰎈 󰏤","class":"playing","title":"$title"}'
+  title=$(playerctl metadata title 2>/dev/null)
+  echo "{\"text\":\"󰎈 󰏤\",\"class\":\"playing\",\"title\":\"$title\"}"
 elif [[ $playerctl_status == "Paused" ]]; then
-    title=$(playerctl metadata title 2>/dev/null)
-    echo '{"text":"󰎈 󰐊","class":"paused","title":"$title"}'
+  title=$(playerctl metadata title 2>/dev/null)
+  echo "{\"text\":\"󰎈 󰐊\",\"class\":\"paused\",\"title\":\"$title\"}"
 else
-    echo '{"text":""}'
+  echo '{"text":""}'
 fi
